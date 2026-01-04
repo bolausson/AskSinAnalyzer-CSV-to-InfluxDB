9c04d09860121ca5cbea77312680a98ecd4155f6
B. Olausson <contactme@olausson.de>
2026-01-04 18:15:09 +0100

    Add VictoriaMetrics support
    - Add asa-to-vm.py script for importing AskSin Analyzer CSV data into VictoriaMetrics
    - Add AskSin-Grafana-Dashboard-VM.json for VictoriaMetrics/Prometheus data source
    - Update README.md with documentation for both InfluxDB and VictoriaMetrics options
    - Add .gitignore to exclude config files and analyzer data

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # AskSinAnalyzer-CSV-to-InfluxDB
-Python script which will read CSV files from [AskSin Analyzer XS](https://github.com/psi-4ward/AskSinAnalyzerXS) containing Homematic IP Telegram information (like DutyCycle) to InfluxDB
+Python scripts to read CSV files from [AskSin Analyzer XS](https://github.com/psi-4ward/AskSinAnalyzerXS) containing Homematic IP Telegram information (like DutyCycle) and import them into InfluxDB or VictoriaMetrics.
+
+## InfluxDB Import (asa-to-ifdb.py)
 
 ```
 usage: asa-to-ifdb.py [-h] [-cc] [-cf CONFIGFILE] [-d CSVDIR] [-f CSVFILES] [-t] [-l]
@@ -29,5 +31,42 @@ Crontab example:
 # Read AskSinAnalyzer CSV to InfluxDB
 */2 * * * * /usr/local/bin/asa-to-ifdb.py -d /opt/analyzer -l >> /dev/null
 ```
+
+## VictoriaMetrics Import (asa-to-vm.py)
+
+```
+usage: asa-to-vm.py [-h] [-cc] [-cf CONFIGFILE] [-d CSVDIR] [-f CSVFILES] [-t] [-l]
+
+Write data from AskSin Analyzer XS CSV files into VictoriaMetrics
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -cc, --create-config
+                        Create configuration file containing the VictoriaMetrics settings and quit.
+                         Name and path can be changed from default (${HOME}/.asa-to-vm.conf)
+                         to something else with "-cf"
+  -cf CONFIGFILE, --config-file CONFIGFILE
+                        VictoriaMetrics configuration file
+  -d CSVDIR, --dir CSVDIR
+                        Directory containing the CSV files - all TelegramsXS_*.csv files in this
+                        dir will be processed (e.g. /opt/analyzer)
+  -f CSVFILES, --files CSVFILES
+                        Specify the path of a file to be processed.
+                        This argument can be used multiple times to process multiple files
+  -t, --test            Do not write any data to VictoriaMetrics - just echo the data to stdout
+  -l, --latest          Only read the most recent file (useful for e.g. cronjobs)
+```
+
+Crontab example:
+```
+# Read AskSinAnalyzer CSV to VictoriaMetrics
+*/2 * * * * /usr/local/bin/asa-to-vm.py -d /opt/analyzer -l >> /dev/null
+```
+
+## Grafana Dashboards
+
+Two Grafana dashboard JSON files are provided:
+- `AskSin-Grafana-Dashboard.json` - Dashboard for InfluxDB data source
+- `AskSin-Grafana-Dashboard-VM.json` - Dashboard for VictoriaMetrics/Prometheus data source
 
 ![Grafana Dashboard](https://github.com/bolausson/AskSinAnalyzer-CSV-to-InfluxDB/blob/main/AskSin-Grafana-Dashboard.png?raw=true)
---
+++ asa-to-vm.py
@@ -0,0 +1,303 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# -----------------------------------------------------------------------------
+# asa-to-vm.py, Copyright Bjoern Olausson
+# -----------------------------------------------------------------------------
+#
+# This tool is inteded to read CSV files containing data gathered by
+# AskSin Analyzer XS and write them as time series into VictoriaMetrics
+#
+
+import os
+import csv
+import sys
+import time
+import argparse
+import configparser
+import requests
+from glob import glob
+from pathlib import Path
+from pprint import pprint
+from datetime import datetime
+from os.path import expanduser
+
+default_config_file = expanduser("~/.asa-to-vm.conf")
+
+parser = argparse.ArgumentParser(description='Write data from AskSin Analyzer XS CSV files into VictoriaMetrics')
+
+parser.add_argument('-cc', '--create-config', dest='createconfigfile', action='store_true', default=False,
+                    help=f'Create configuration file containing the VictoriaMetrics credentials and quit. Name and path can be changed from default ({default_config_file}) to something else with "-c"')
+parser.add_argument('-cf', '--config-file', dest='configfile', default=default_config_file,
+                    help='VictoriaMetrics credential file')
+
+parser_group_1 = parser.add_argument_group('Options')
+parser.add_argument('-d', '--dir', dest='csvdir', default="", type=str,
+                    help='Directory containing the CSV files - all TelegramsXS_*.csv files in this dir will be processed (e.g. /opt/analyzer)')
+parser.add_argument('-f', '--files', dest='csvfiles', default=[], type=str, action='append',
+                    help='Specify the path of a file to be processed. This argument can be used multiple times to process multiple files')
+parser.add_argument('-t', '--test', dest='dryrun', action='store_true', default=False,
+                    help='Do not write any data to VictoriaMetrics - just echo the data to stdout')
+parser.add_argument('-l', '--latest', dest='latest', action='store_true', default=False,
+                    help='Only read the most recent file (usefull for e.g. cronjobs)')
+
+args = parser.parse_args()
+config = configparser.ConfigParser()
+
+CREATECONFIGFILE = args.createconfigfile
+CONFIGFILE = args.configfile
+CSVDIR = args.csvdir
+CSVFILES = args.csvfiles
+DRYRUN = args.dryrun
+LATEST = args.latest
+
+if Path(CONFIGFILE).is_file() and CREATECONFIGFILE:
+    print(f'Configuration file "{CONFIGFILE}" already exists!')
+    print(f'Use a different name/path (with -cf) or delete "{CONFIGFILE}" to create a new default config')
+    sys.exit()
+elif not Path(CONFIGFILE).is_file() and CREATECONFIGFILE:
+    config['VM'] = {
+        'VM_URL': "Your-URL-to-VictoriaMetrics Goes Here e.g. http://localhost",
+        'VM_PORT': 8428,
+        'VM_VERIFY_SSL': False,
+        'VM_USERNAME': "Optional: Your-Username-Goes-Here (leave empty if not using auth)",
+        'VM_PASSWORD': "Optional: Your-Password-Goes-Here (leave empty if not using auth)",
+        'VM_RANGE_START': "Start looking back for last result e.g. 30d or 365d",
+    }
+
+    with open(CONFIGFILE, 'w') as f:
+        config.write(f)
+        print(f'Configuration file {CONFIGFILE} created!')
+        sys.exit()
+else:
+    config.read(CONFIGFILE)
+
+if not CSVDIR and not CSVFILES:
+    print("Either -d or -f must be specified!")
+    print("Use -h for help")
+    sys.exit()
+
+VM_URL = config['VM']['VM_URL']
+VM_PORT = config['VM']['VM_PORT']
+VM_RANGE_START = config['VM'].get('VM_RANGE_START', '30d')
+VM_USERNAME = config['VM'].get('VM_USERNAME', '')
+VM_PASSWORD = config['VM'].get('VM_PASSWORD', '')
+
+if config['VM']['VM_VERIFY_SSL'].lower() in ['true', '1']:
+    VM_VERIFY_SSL = True
+else:
+    VM_VERIFY_SSL = False
+
+current_time = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+# VictoriaMetrics endpoints
+VM_IMPORT_URL = f'{VM_URL}:{VM_PORT}/api/v1/import/prometheus'
+VM_QUERY_URL = f'{VM_URL}:{VM_PORT}/api/v1/query'
+
+# Setup authentication if configured
+VM_AUTH = None
+if VM_USERNAME and VM_PASSWORD:
+    VM_AUTH = (VM_USERNAME, VM_PASSWORD)
+
+# Query VictoriaMetrics for the last timestamp
+last_ts_value = 0
+
+def get_last_timestamp():
+    """Query VictoriaMetrics for the last processed timestamp"""
+    global last_ts_value
+
+    # Query for the maximum timestamp of any Telegrams metric
+    # Using last_over_time to get the most recent data point
+    query = f'last_over_time(Telegrams_cnt[{VM_RANGE_START}])'
+
+    attempts = 3
+    for attempt in range(attempts):
+        try:
+            t0 = time.time()
+            response = requests.get(
+                VM_QUERY_URL,
+                params={'query': query},
+                auth=VM_AUTH,
+                verify=VM_VERIFY_SSL,
+                timeout=60
+            )
+            response.raise_for_status()
+            t1 = time.time()
+            totalt = t1 - t0
+            print(f'VictoriaMetrics query took {totalt:.2f} seconds')
+
+            data = response.json()
+            if data.get('status') == 'success' and data.get('data', {}).get('result'):
+                # Find the maximum timestamp from all results
+                # Timestamp is in seconds (float), convert to milliseconds
+                max_ts = 0
+                for result in data['data']['result']:
+                    ts_seconds = float(result['value'][0])
+                    ts_ms = int(ts_seconds * 1000)
+                    if ts_ms > max_ts:
+                        max_ts = ts_ms
+                last_ts_value = max_ts
+                print(f'Last timestamp in VictoriaMetrics: {last_ts_value}')
+            else:
+                print('No existing data found in VictoriaMetrics')
+            return
+
+        except requests.exceptions.ConnectionError as e:
+            print(f'Warning: Could not connect to VictoriaMetrics (attempt {attempt + 1}/{attempts}): {e}', file=sys.stderr)
+            time.sleep(10)
+        except Exception as e:
+            print(f'Warning: Error querying VictoriaMetrics (attempt {attempt + 1}/{attempts}): {e}', file=sys.stderr)
+            time.sleep(10)
+
+    print(f'All {attempts} attempts to query VictoriaMetrics failed - starting from scratch', file=sys.stderr)
+
+# Only query for last timestamp if using -l/--latest flag
+if LATEST:
+    get_last_timestamp()
+
+def escape_label_value(value):
+    """Escape special characters in Prometheus label values"""
+    return str(value).replace('\\', '\\\\').replace('"', '\\"').replace('\n', '\\n')
+
+def format_prometheus_line(measurement, tags, fields, timestamp_ms):
+    """Format a data point as Prometheus line protocol for VictoriaMetrics"""
+    lines = []
+    for field_name, field_value in fields.items():
+        if field_name in ['tstamp', 'date']:  # Skip non-numeric fields
+            continue
+        # Build label string
+        label_parts = [f'{k}="{escape_label_value(v)}"' for k, v in tags.items()]
+        labels_str = ','.join(label_parts)
+        # Metric name format: measurement_field{labels} value timestamp
+        metric_name = f'{measurement}_{field_name}'
+        lines.append(f'{metric_name}{{{labels_str}}} {field_value} {timestamp_ms}')
+    return lines
+
+CSVFILELIST = []
+FIELDNAMES = ['tstamp', 'date', 'rssi', 'len', 'cnt', 'dc', 'flags', 'type', 'fromAddr', 'toAddr', 'fromName', 'toName', 'fromSerial', 'toSerial', 'toIsIp', 'fromIsIp', 'payload', 'raw']
+
+if CSVDIR:
+    print("Globbing files to be processed")
+    CSVFILESINDIR = glob(f"{CSVDIR}/TelegramsXS_*.csv")
+    CSVFILELIST.extend(CSVFILESINDIR)
+
+if len(CSVFILES) > 0:
+    print("Files to be processed")
+    CSVFILELIST.extend(CSVFILES)
+
+SORTEDCSVFILELIST = sorted(CSVFILELIST, key=os.path.getmtime)
+
+if LATEST:
+    SORTEDCSVFILELIST = [SORTEDCSVFILELIST[-1]]
+    print(f'Only using the latest file: {SORTEDCSVFILELIST[0]}')
+
+NUMFILES = len(SORTEDCSVFILELIST)
+COUNTER = 1
+max_ts_processed = last_ts_value
+
+for file in SORTEDCSVFILELIST:
+    print(f'Reading file {file} ({COUNTER}/{NUMFILES})')
+    COUNTER += 1
+    PROMETHEUS_LINES = []
+
+    with open(file) as f:
+        try:
+            reader = csv.DictReader(f, delimiter=';', fieldnames=FIELDNAMES)
+        except Exception as e:
+            print(f'{current_time}', file=sys.stderr)
+            print("-----------------------------------------------------", file=sys.stderr)
+            print(f'File {file} could not be read by csv.DictReader! Skipping it!', file=sys.stderr)
+            print(e, file=sys.stderr)
+            print("-----------------------------------------------------", file=sys.stderr)
+
+        else:
+            rowcounter = 1
+            try:
+                for row in reader:
+                    rowcounter += 1
+                    # Do not process the header line!
+                    if row["tstamp"] != "tstamp":
+                        try:
+                            ts = int(row["tstamp"])
+
+                            tags = {
+                                "flags": str(row["flags"]),
+                                "type": str(row["type"]),
+                                "fromAddr": str(row["fromAddr"]),
+                                "toAddr": str(row["toAddr"]),
+                                "fromName": str(row["fromName"]),
+                                "toName": str(row["toName"]),
+                                "fromSerial": str(row["fromSerial"]),
+                                "toSerial": str(row["toSerial"]),
+                                "toIsIp": str(row["toIsIp"]),
+                                "fromIsIp": str(row["fromIsIp"]),
+                            }
+
+                            fields = {
+                                "rssi": int(row["rssi"]),
+                                "len": int(row["len"]),
+                                "cnt": int(row["cnt"]),
+                                "dc": float(row["dc"]),
+                            }
+
+                            if LATEST and ts <= last_ts_value:
+                                pass
+                            else:
+                                lines = format_prometheus_line("Telegrams", tags, fields, ts)
+                                PROMETHEUS_LINES.extend(lines)
+                                if ts > max_ts_processed:
+                                    max_ts_processed = ts
+                        except (ValueError, KeyError) as e:
+                            # Skip rows with invalid data (e.g., null bytes from corrupted files)
+                            print(f'Warning: Skipping invalid row {rowcounter} in {file}: {e}', file=sys.stderr)
+                            continue
+            except (csv.Error) as e:
+                print(f'{current_time}', file=sys.stderr)
+                print("-----------------------------------------------------", file=sys.stderr)
+                print(f'File {file} is broken! Skipping it!', file=sys.stderr)
+                print(f'Row: {rowcounter}', file=sys.stderr)
+                print(e, file=sys.stderr)
+                print("-----------------------------------------------------", file=sys.stderr)
+
+    if DRYRUN:
+        for line in PROMETHEUS_LINES[:20]:  # Print first 20 lines as sample
+            print(line)
+        if len(PROMETHEUS_LINES) > 20:
+            print(f'... and {len(PROMETHEUS_LINES) - 20} more lines')
+    else:
+        if len(PROMETHEUS_LINES):
+            print(f"Writing {len(PROMETHEUS_LINES)} data points to VictoriaMetrics")
+            data = '\n'.join(PROMETHEUS_LINES)
+
+            try:
+                t0 = time.time()
+                auth = None
+                if VM_USERNAME and VM_PASSWORD:
+                    auth = (VM_USERNAME, VM_PASSWORD)
+
+                response = requests.post(
+                    VM_IMPORT_URL,
+                    data=data,
+                    headers={'Content-Type': 'text/plain'},
+                    auth=auth,
+                    verify=VM_VERIFY_SSL,
+                    timeout=600
+                )
+                response.raise_for_status()
+                t1 = time.time()
+                totalt = t1 - t0
+                print(f'VictoriaMetrics write took {totalt:.2f} seconds')
+
+            except Exception as e:
+                t1 = time.time()
+                totalt = t1 - t0
+                print(f'{current_time}', file=sys.stderr)
+                print(f'VictoriaMetrics write took {totalt:.2f} seconds before failing!', file=sys.stderr)
+                print("-----------------------------------------------------", file=sys.stderr)
+                print(f'Failed to write to VictoriaMetrics', file=sys.stderr)
+                print(e, file=sys.stderr)
+                print("-----------------------------------------------------", file=sys.stderr)
+                sys.exit()
+        else:
+            print("VictoriaMetrics is up to date, there is no new data to write")
+
